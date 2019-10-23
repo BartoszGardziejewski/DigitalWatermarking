7b3b2fde48fb0c6bc1a9820165bd6de9c6af5d86
lukaszobrebski <lukasz.obrebski95@gmail.com>
2019-10-23 17:51:44 +0200

    Fixed path in convertImageToMasksAndSave. Added changeLuminanceOfPixel in PatchworkUtils

--- python/src/common/ImageManagement.py
+++ python/src/common/ImageManagement.py
@@ -1,4 +1,5 @@
 from skimage import io
+from skimage import color
 
 #Does its work in resources folder
 def convertImageToMasksAndSave(fileName = "python.bmp"):
@@ -10,8 +11,14 @@ def convertImageToMasksAndSave(fileName = "python.bmp"):
     imageG = image[..., 1]
     imageB = image[..., 2]
 
-    io.imsave("./resources/" + fileName.replace(".","R."), imageR)
-    io.imsave("./resources/" + fileName.replace(".","G."), imageG)
-    io.imsave("./resources/" + fileName.replace(".","B."), imageB)
+    io.imsave("../../resources/" + fileName.replace(".","R."), imageR)
+    io.imsave("../../resources/" + fileName.replace(".","G."), imageG)
+    io.imsave("../../resources/" + fileName.replace(".","B."), imageB)
+
+#Loads from resource folder
+def loadImageAndConvertToYUV(fileName = "python.bmp"):
+    image = io.imread("./resources/" + fileName)
+    return color.rgb2yuv(image)
+
 
 
---
+++ python/src/patchwork/PatchworkUtils.py
@@ -0,0 +1,6 @@
+from skimage import io
+
+
+def changeLuminanceOfPixel(image, x, y, value):
+    image[x,y,0] += value
+    return image
