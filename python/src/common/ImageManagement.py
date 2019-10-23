150b67d5ae2669f749cb2c384ada2058724a93ac
lukaszobrebski <lukasz.obrebski95@gmail.com>
2019-10-23 18:50:37 +0200

    Added required P1 task functionality, loadImage func and fixed path again. Tested in Algorithm.py

--- python/src/common/ImageManagement.py
+++ python/src/common/ImageManagement.py
@@ -3,22 +3,25 @@ from skimage import color
 
 #Does its work in resources folder
 def convertImageToMasksAndSave(fileName = "python.bmp"):
-    image = io.imread("./resources/" + fileName)
-
-    print(image.shape)
+    baseDir = "../../resources"
+    image = io.imread(baseDir + fileName)
 
     imageR = image[..., 0]
     imageG = image[..., 1]
     imageB = image[..., 2]
 
-    io.imsave("../../resources/" + fileName.replace(".","R."), imageR)
-    io.imsave("../../resources/" + fileName.replace(".","G."), imageG)
-    io.imsave("../../resources/" + fileName.replace(".","B."), imageB)
+    io.imsave(baseDir + fileName.replace(".","R."), imageR)
+    io.imsave(baseDir + fileName.replace(".","G."), imageG)
+    io.imsave(baseDir + fileName.replace(".","B."), imageB)
 
 #Loads from resource folder
 def loadImageAndConvertToYUV(fileName = "python.bmp"):
-    image = io.imread("./resources/" + fileName)
+    image = io.imread("../../resources/" + fileName)
     return color.rgb2yuv(image)
+
+#Loads from resources folder
+def loadImage(fileName = "python.bmp"):
+    return io.imread("../../resources/" + fileName)
 
 
 
